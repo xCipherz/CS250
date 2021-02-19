480471212163d52f9fd10aa27a739d1ae17bbe47
Princess Wanderlust <ethnharper@gmail.com>
2021-02-19 10:03:35 -0600

    Update ChessBoard.cpp

--- N Queens/ChessBoard.cpp
+++ N Queens/ChessBoard.cpp
@@ -1,1 +1,43 @@
+/**
+* Ethan Harper
+* ChessBoard.cpp
+**/
 
+#include <iostream>
+//#include <string>
+#include "ChessBoard.h"
+
+ChessBoard::ChessBoard()
+{
+
+}
+ChessBoard::ChessBoard(int)
+{
+
+}
+ChessBoard::~ChessBoard()  //Destructor
+{
+
+}
+void ChessBoard::setSize(int)
+{
+    
+}
+int ChessBoard::getSize()
+{
+    return -1;
+}
+bool ChessBoard::solve()
+{
+    return false;
+}
+void ChessBoard::displayBoard()
+{
+
+}
+bool ChessBoard::placeQueens(int)
+{
+    return false;
+}
+//char** boardPtr;
+//int boardSize;
