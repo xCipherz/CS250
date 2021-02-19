763bd4e1b2e8851ad6b060e33af7c50cae900d2f
Princess Wanderlust <ethnharper@gmail.com>
2021-02-19 09:43:35 -0600

    Update ChessBoard.h

--- N Queens/ChessBoard.h
+++ N Queens/ChessBoard.h
@@ -1,1 +1,29 @@
+/**
+* Ethan Harper
+* ChessBoard.h
+**/
+
+#pragma once
+#ifndef CHESSBOARD_H
+#define CHESSBOARD_H
+
+using namespace std;
+
+class ChessBoard
+{
+public:
+	ChessBoard();
+    ChessBoard(int);
+	~ChessBoard();  //Destructor
+    void setSize(int);
+    int getSize();
+    bool solve();
+    void displayBoard();
+
+private:
+    char** boardPtr;
+    int boardSize;
+    bool placeQueens(int);
+};
+#endif
 
