28fd8cbebbe64c295b1763916bfac61b50053256
Princess Wanderlust <ethnharper@gmail.com>
2021-02-19 10:32:00 -0600

    Update ChessBoard.cpp

--- N Queens/ChessBoard.cpp
+++ N Queens/ChessBoard.cpp
@@ -9,23 +9,66 @@
 
 ChessBoard::ChessBoard()
 {
-
+    setSize(8);
 }
-ChessBoard::ChessBoard(int)
+ChessBoard::ChessBoard(int size)
 {
-
+    setSize(size);
 }
 ChessBoard::~ChessBoard()  //Destructor
 {
-
+    deleteBoard();
 }
-void ChessBoard::setSize(int)
+
+void ChessBoard::deleteBoard()
 {
-    
+    if (boardPtr != nullptr)
+	{
+		for (int i = 0; i < boardSize; i++)
+		{
+			delete[] boardPtr[i];
+		}
+
+		delete[] boardPtr;
+	}
+}
+
+void ChessBoard::setSize(int size)
+{
+    if (boardPtr != nullptr) //If board exists, delete it
+	{
+		deleteBoard();
+	}
+
+    if(size > 12)
+    {
+        size = 12;
+    }
+    else if (size < 4)
+    {
+        size = 4;
+    }
+
+    boardPtr = new char* [size]; //Create a new board of given size
+
+	for (int i = 0; i < size; i++)
+	{
+		boardPtr[i] = new char[size];
+	}
+
+    for (int i = 0; i < size; i++) //Initialize the table to all asterisks
+	{
+		for (int j = 0; j < size; j++)
+		{
+		    boardPtr[i][j] = '*';
+		}
+	}
+
+    boardSize = size; //Reset board size
 }
 int ChessBoard::getSize()
 {
-    return -1;
+    return boardSize;
 }
 bool ChessBoard::solve()
 {
